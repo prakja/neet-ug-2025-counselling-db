80ccc690eb7380ce273b13ca6c085c26e185e8bd
Jay Prakash <jprakash@goodeducator.com>
2026-05-01 12:39:13 +0530

    feat: add GitHub Actions CI/CD + switch ECS to X86_64 for standard GitHub runner support

--- deploy/main.tf
+++ deploy/main.tf
@@ -144,7 +144,7 @@ resource "aws_ecs_task_definition" "counselling" {
 
   runtime_platform {
     operating_system_family = "LINUX"
-    cpu_architecture        = "ARM64"
+    cpu_architecture        = "X86_64"
   }
 
   container_definitions = jsonencode([
